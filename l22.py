c73c8383a58c092a27d3f7f5b4982db9b8d08001
qiaosiyi <qsy2011815@sina.com>
2015-06-01 22:51:25 +0800

    Create l22.py
    And this App can passer out the  ipv4_src/dst infomation from a ipv4 packetin message
    I want to use the ipv4_dst to route this ipv4 flow in a small SDN network

---
+++ l22.py
@@ -0,0 +1,104 @@
+# Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+####################################################################################################
+##   I learned from the python script 'l2.py' written in ryuDoc3.20 
+##   http://ryu.readthedocs.org/en/latest/writing_ryu_app.html
+##   Run l22.py by type "sudo ryu-manager l22.py"
+##   And this App can passer out the  ipv4_src/dst infomation from a ipv4 packetin message
+##   I want to use the ipv4_dst to route this ipv4 flow in a small SDN network
+####################################################################################################
+
+
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import udp
+from ryu.lib.packet import ipv4
+
+
+
+class SimpleSwitch13(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(SimpleSwitch13, self).__init__(*args, **kwargs)
+        #self.mac_to_port = {}
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # install table-miss flow entry
+        #
+        # We specify NO BUFFER to max_len of the output action due to
+        # OVS bug. At this moment, if we specify a lesser number, e.g.,
+        # 128, OVS will send Packet-In with invalid buffer_id and
+        # truncated packet data. In that case, we cannot output packets
+        # correctly.  The bug has been fixed in OVS v2.1.0.
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+        print "Install table-miss flow entry to dp:",datapath.id##qsy
+
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match,
+                                    instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
+        datapath.send_msg(mod)
+        print "sending flowentry to dp:",datapath.id##qsy
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn,MAIN_DISPATCHER)
+    def packet_in_handler(self,ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+        pkt = packet.Packet(msg.data)
+        
+        print"=================================="
+
+        ipv4_hdr = pkt.get_protocols(ipv4.ipv4)[0]
+        dst = ipv4_hdr.dst
+        src = ipv4_hdr.src
+        dpid = datapath.id
+
+
+        # actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
+        # out = parser.OFPPacketOut(
+        #     datapath=datapath,buffer_id=msg.buffer_id,in_port=msg.match['in_port'],
+        #     actions=actions)
+        
+
+        # datapath.send_msg(out)
+        print "dpid:",dpid,"in_port:",in_port,"src:",src,"dst:",dst
+        pass
